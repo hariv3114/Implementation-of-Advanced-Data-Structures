34d69e08483d6f297dccf5283091050423df5afa
Harigarakumar <hariv3114>
2016-03-05 16:21:25 -0600

    Comments

--- HackerRank/HackerRank/src/hackerrank/ProfessorsProblem.java
+++ HackerRank/HackerRank/src/hackerrank/ProfessorsProblem.java
@@ -1,9 +1,11 @@
 package hackerrank;
 
+import java.util.Scanner;
+
 /**
  * 
  * A Discrete Mathematics professor has a class of NN students. Frustrated with
- * their lack of discipline, he decides to cancel class if fewer than KK
+ * their lack of discipline, he decides to cancel class if fewer than K
  * students are present when class starts.
  * 
  * Given the arrival time of each student, determine if the class is canceled.
@@ -13,13 +15,13 @@ package hackerrank;
  * The first line of input contains TT, the number of test cases.
  * 
  * Each test case consists of two lines. The first line has two space-separated
- * integers, NN (students in the class) and KK (the cancelation threshold). The
+ * integers, N (students in the class) and K (the cancellation threshold). The
  * second line contains NN space-separated integers (a1,a2,…,aNa1,a2,…,aN)
  * describing the arrival times for each student.
  * 
- * Note: Non-positive arrival times (ai≤0ai≤0) indicate the student arrived
- * early or on time; positive arrival times (ai>0ai>0) indicate the student
- * arrived aiai minutes late.
+ * Note: Non-positive arrival times (ai≤0) indicate the student arrived
+ * early or on time; positive arrival times (ai>0) indicate the student
+ * arrived ai minutes late.
  * 
  * Output Format
  * 
@@ -35,7 +37,11 @@ package hackerrank;
  * 
  * Sample Input
  * 
- * 2 4 3 -1 -3 4 2 4 2 0 -1 2 1 
+ * 2
+ * 4 3
+ * -1 -3 4 2
+ * 4 2 
+ * 0 -1 2 1 
  * 
  * Sample Output
  * YES N0
@@ -46,7 +52,34 @@ public class ProfessorsProblem {
 	
 	public static void main(String args[]){
 		
+		Scanner input = new Scanner(System.in);
+		int t = input.nextInt();
 		
+		// Reiterate T times the input loop to gather all the test cases
+		// I intend to printout the Result inside the loop
+		
+		for(int t_i=0;t_i < t; t_i++){
+			
+			int n = input.nextInt();
+			int k = input.nextInt();
+			int attnd_count = 0;
+			// Gather attendance values for this test case
+			for(int n_i=0; n_i < n;n_i++){
+				int next = input.nextInt();
+				if(next <= 0){
+					attnd_count++;
+				}
+			}
+			if(attnd_count < k){
+				System.out.println("YES");
+			}else{
+				System.out.println("NO");
+			}
+			
+		}
+		
+		// close the Scanner
+		input.close();
 	}
 	
 
