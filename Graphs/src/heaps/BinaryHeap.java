dad611ae78fcc197d3ac4e4ead9b47d7b7cd0c3c
Harigarakumar <hariv3114>
2016-03-22 19:16:35 -0500

    BinaryHeap updated with cons. impl.

--- Graphs/src/heaps/BinaryHeap.java
+++ Graphs/src/heaps/BinaryHeap.java
@@ -1,63 +1,81 @@
-// Ver 1.0:  Wec, Feb 3.  Initial description.
- 
+// Ver 1.0:  Wed, Feb 3.  Initial description.
 package heaps;
- 
+
+import java.lang.reflect.Array;
 import java.util.Comparator;
 
 public class BinaryHeap<T> implements PQ<T> {
-    T[] pq;
-    Comparator<T> c;
-    /** Build a priority queue with a given array q */
-    BinaryHeap(T[] q, Comparator<T> comp) {
-	pq = q;
-	c = comp;
-	buildHeap();
-    }
+	T[] pq;
+	Comparator<T> c;
+	int sizeOfHeap;
 
-    /** Create an empty priority queue of given maximum size */
-    BinaryHeap(int n, Comparator<T> comp) { /* to be implemented */
-    }
+	/** Build a priority queue with a given array q */
+	BinaryHeap(T[] q, Comparator<T> comp) {
+		pq = q;
+		c = comp;
+		//update Size of Heap
+		sizeOfHeap = pq.length-1;
+		buildHeap();
+	}
 
-    public void insert(T x) {
-	add(x);
-    }
+	/** Create an empty priority queue of given maximum size */
+	BinaryHeap(int n, Comparator<T> comp) { 
+		PQ[] binHeap = new PQ[n];
+		pq = (T[]) binHeap;
+		sizeOfHeap = pq.length -1;
+		c = comp;
+	}
 
-    public T deleteMin() {
-	return remove();
-    }
+	public void insert(T x) {
+		add(x);
+	}
 
-    public T min() { 
-	return peek();
-    }
+	public T deleteMin() {
+		return remove();
+	}
 
-    public void add(T x) { /* to be implemented */
-    }
+	public T min() {
+		return peek();
+	}
 
-    public T remove() { /* to be implemented */
-	return null;
-    }
+	/**
+	 *  Emulates the percolateUp concept of a Heap.Initially added to the leftmost 
+	 *  available leaf spot. Iteratively compared to the parent to check for heap condition
+	 *  and eventually placing it in the proper spot.
+	 *  
+	 *  Heap size increases by 1.
+	 *  
+	 *  @param : x - element to be added to the heap
+	 */
+	public void add(T x) { /* to be implemented */
+	}
 
-    public T peek() { /* to be implemented */
-	return null;
-    }
+	public T remove() { /* to be implemented */
+		return null;
+	}
 
-    /** pq[i] may violate heap order with parent */
-    void percolateUp(int i) { /* to be implemented */
-    }
+	public T peek() { /* to be implemented */
+		return null;
+	}
 
-    /** pq[i] may violate heap order with children */
-    void percolateDown(int i) { /* to be implemented */
-    }
+	/** pq[i] may violate heap order with parent */
+	void percolateUp(int i) { /* to be implemented */
+	}
 
-    /** Create a heap.  Precondition: none. */
-    void buildHeap() {
-    }
+	/** pq[i] may violate heap order with children */
+	void percolateDown(int i) { /* to be implemented */
+	}
 
-    /* sort array A[1..n].  A[0] is not used. 
-       Sorted order depends on comparator used to buid heap.
-       min heap ==> descending order
-       max heap ==> ascending order
-     */
-    public static<T> void heapSort(T[] A, Comparator<T> comp) { /* to be implemented */
-    }
+	/** Create a heap. Precondition: none. */
+	void buildHeap() {
+	}
+
+	/*
+	 * sort array A[1..n]. A[0] is not used. Sorted order depends on comparator
+	 * used to buid heap. min heap ==> descending order max heap ==> ascending
+	 * order
+	 */
+	public static <T> void heapSort(T[] A,
+			Comparator<T> comp) { /* to be implemented */
+	}
 }
---
+++ Graphs/src/heaps/MST.java
@@ -0,0 +1,27 @@
+package heaps;
+
+import java.util.Scanner;
+
+import graphmodels.Graph;
+import graphmodels.Vertex;
+
+public class MST {
+    static final int Infinity = Integer.MAX_VALUE;
+
+    static int PrimMST(Graph g)
+    {
+        int wmst = 0;
+        Vertex src = g.verts.get(1);
+
+        // Code for Prim's algorithm needs to be written
+
+        return wmst;
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        Graph g = Graph.readGraph(in, false);
+        System.out.println(PrimMST(g));
+    }
+}
