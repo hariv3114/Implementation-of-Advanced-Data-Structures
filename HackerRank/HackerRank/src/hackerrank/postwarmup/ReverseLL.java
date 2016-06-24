ed1d5d0e854fb79f256af63809967ec90d071537
Harigarakumar <hariv3114>
2016-06-23 19:19:50 -0500

    Reversal using recursion

--- HackerRank/HackerRank/src/hackerrank/postwarmup/ReverseLL.java
+++ HackerRank/HackerRank/src/hackerrank/postwarmup/ReverseLL.java
@@ -11,16 +11,17 @@ import java.util.List;
  *
  */
 public class ReverseLL {
-	
-	static ReverseLL thisClass = new ReverseLL(); 
-	
-	public static void main(String[] args){
-		
+
+	static ReverseLL thisClass = new ReverseLL();
+	static Node listHead;
+
+	public static void main(String[] args) {
+
 		Node head = thisClass.new Node(1);
 		Node next_1 = thisClass.new Node(2);
 		Node next_2 = thisClass.new Node(3);
 		Node next_3 = thisClass.new Node(4);
-		Node next_4 = thisClass.new Node(5); 
+		Node next_4 = thisClass.new Node(5);
 
 		// Mapping
 		head.next = next_1;
@@ -30,41 +31,40 @@ public class ReverseLL {
 		next_4.next = null;
 
 		thisClass.print(head);
-		thisClass.print(reverse(head));
-		
+		thisClass.print(reverseAlt(head));
+
 	}
 
-	//inner class for node
-	class Node{
+	// inner class for node
+	class Node {
 
 		int data;
 		Node next;
 
-		//Constructor
-		public Node(int data){
+		// Constructor
+		public Node(int data) {
 			this.data = data;
 			this.next = null;
 		}
 
-		//Constructor with next and data set
-		Node(int data, Node next){
+		// Constructor with next and data set
+		Node(int data, Node next) {
 			this.data = data;
 			this.next = next;
 		}
 	}
 
-
 	/***
-	* Method to reverse a LinkedList
-	*
-	* @param : LinkedList to be reversed
-	* @return : reversed LinkedList
-	*/
-	public static Node reverse(Node head){
-
+	 * Method to reverse a LinkedList
+	 *
+	 * @param :
+	 *            LinkedList to be reversed
+	 * @return : reversed LinkedList
+	 */
+	public static Node reverse(Node head) {
 
 		// Single node or no node check
-		if(head.next == null || head == null){
+		if (head.next == null || head == null) {
 			return head;
 		}
 
@@ -72,12 +72,10 @@ public class ReverseLL {
 		Node curr = head.next;
 		Node next = head.next.next;
 
-		//Set current head to null
+		// Set current head to null
 		head.next = null;
 
-		Node temp;
-
-		while(next.next != null){
+		while (next.next != null) {
 
 			curr.next = prev;
 
@@ -91,25 +89,66 @@ public class ReverseLL {
 
 		return next;
 	}
-	
+
 	/**
-	 *  Print all the nodes of a Linked List 
+	 * Print all the nodes of a Linked List
 	 * 
 	 * @param head
 	 * 
 	 */
-	
-	void print(Node head){
+
+	void print(Node head) {
 
 		Node node = head;
 
-		while(node.next != null){
-			System.out.print(node.data+", ");
+		while (node != null) {
+			System.out.print(node.data + ", ");
 			node = node.next;
 		}
 
-		System.out.print(node.data);
 		System.out.println();
+
+	}
+
+	public static Node reverseAlt(Node head) {
+
+		Node nextToHead = revByRecurse(head.next);
+		nextToHead.next = head;
+		head.next = null;
+
+		return listHead;
+
+	}
+
+	/***
+	 * Recursively reverses the LinkedList
+	 * 
+	 * @param Node
+	 *            pointing to head
+	 * @return Node pointing to next
+	 */
+
+	public static Node revByRecurse(Node currNode) {
+
+		Node prev;
+
+		if (currNode == null || currNode.next == null) {
+			return currNode;
+		}
+
+		if (currNode.next.next == null) {
+			prev = currNode;
+			currNode = currNode.next;
+			listHead = currNode;
+			currNode.next = prev;
+
+		} else {
+			prev = revByRecurse(currNode.next);
+			prev.next = currNode;
+			prev = currNode;
+		}
+
+		return prev;
 
 	}
 }
